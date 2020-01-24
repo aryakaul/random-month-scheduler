87843c4f65b351cb0502240f880bae53147402c8
luak <arya_kaul@g.harvard.edu>
2020-01-24 12:02:43 -0500

    added name + event counter + avgeventspermonth

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     month_chosen = None
     while month_chosen == None:
         month_chosen = input(
-            "What month would you like to plan recurring, random events for? Choose between 01-12. \n"
+            "What month would you like to plan recurring, random events for? [01-12] \n"
         )
         try:
             month_chosen = int(month_chosen)
@@ -18,7 +18,27 @@ def main():
         except:
             print("Invalid option. Please choose a number between 1 and 12\n\n")
             month_chosen = None
+    print("\nMonth chosen: %s" % calendar.month_abbr[month_chosen])
 
+    while True:
+        numeventsplanned = 1
+        
+        name = input("\nName of recurring event #%s? \n" % numeventsplanned)
+        eventnumpermonth = None
+        
+        while eventnumpermonth == None:
+            eventnumpermonth = input(
+                "How many of these events per month on average? \n"
+            )
+            try:
+                eventnumpermonth = int(eventnumpermonth)
+                if eventnumpermonth < 1:
+                    raise Exception
+            except:
+                print("Invalid option. Choose a valid natural number > 1\n\n")
+                eventnumpermonth = None
+        
+        
 
 if __name__ == "__main__":
     main()
