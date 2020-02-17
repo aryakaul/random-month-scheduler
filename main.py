e68efec766de228c958512950369259860f24daa
luak <arya_kaul@g.harvard.edu>
2020-02-17 15:15:05 -0500

    alpha version

--- main.py
+++ main.py
@@ -2,30 +2,54 @@
 
 import datetime
 import calendar
+import numpy as np
+import random
 
 def get_num_weekends(year, month, monthrange):
     num = 0
     for days in range(1,monthrange[1]+1):
         x = calendar.weekday(year, month, days)
         if x > 4:
+            # hence weekend
             num += 1
     return num
 
-def select_days(month, weekend_scaling, avg_events):
+def get_probabilities(month, weekend_scaling, avg_events):
     curr_year = datetime.datetime.now().year
     range_in_given_month = calendar.monthrange(curr_year,month)
     num_weekends = get_num_weekends(curr_year,month,range_in_given_month)
-    print(num_weekends)
-    for days in range(1,range_in_given_month[1]+1):
+    num_weekdays = range_in_given_month[1]-num_weekends
+    lhs = np.array([[num_weekends, num_weekdays], [1, -weekend_scaling]])
+    rhs = np.array([avg_events,0])
+    soln = np.linalg.solve(lhs, rhs)
+    pr_weekend = soln[0]
+    pr_weekday = soln[1]
+    return (pr_weekend, pr_weekday)
+
+def select_days(probabilities, month):
+    curr_year = datetime.datetime.now().year
+    num_days = calendar.monthrange(curr_year,month)[1]
+    pr_weekend = probabilities[0]
+    pr_weekday = probabilities[1]
+    dates_to_add = []
+    for days in range(1, num_days+1):
         x = calendar.weekday(curr_year, month, days)
+        flip = random.random()
+        if x > 4:
+            if flip<pr_weekend:
+                dates_to_add.append(days)
+        else:
+            if flip<pr_weekday:
+                dates_to_add.append(days)
+    return dates_to_add
+
 
 def main():
-    select_days(2,2,3)
-    print("\n\nWelcome. \n")
+    print("\nWelcome. \n")
     month_chosen = None
     while month_chosen == None:
         month_chosen = input(
-            "What month would you like to plan recurring, random events for? [01-12] \n"
+            "What month would you like to plan recurring, random events for? [01-12] \n\t"
         )
         try:
             month_chosen = int(month_chosen)
@@ -34,17 +58,19 @@ def main():
         except:
             print("Invalid option. Please choose a number between 1 and 12\n\n")
             month_chosen = None
-    print("Month chosen: %s" % calendar.month_abbr[month_chosen])
+    print("Month chosen: %s" % calendar.month_name[month_chosen])
 
+    numeventsplanned = 1
+    eventnamevect = []
+    eventdatevect = []
     while True:
-        numeventsplanned = 1
-
-        name = input("\nName of recurring event #%s? \n" % numeventsplanned)
+        name = input("\nName of recurring event #%s? Type (q) to quit\n\t" % numeventsplanned)
+        if name == 'q': break
 
         eventnumpermonth = None
         while eventnumpermonth == None:
             eventnumpermonth = input(
-                "\nHow many of event #%s per month on average? \n" % numeventsplanned
+                "\nHow many times would you like to '%s' on average for the chosen month? \n\t" % name
             )
             try:
                 eventnumpermonth = int(eventnumpermonth)
@@ -76,7 +102,7 @@ def main():
         weekend_scaling = None
         while weekend_scaling == None:
             weekend_scaling = input(
-                "\nHow much more likely do you want to do this event on a weekend as opposed to a weekday? \n" % eventprob_weekend
+                "\nHow much more likely do you want to do this event on a weekend as opposed to a weekday? \n\t"
             )
             try:
                 weekend_scaling = float(weekend_scaling)
@@ -85,6 +111,21 @@ def main():
             except:
                 print("Invalid option. Choose a rational number > 0\n\n")
                 weekend_scaling = None
+
+        probabilities = get_probabilities(month_chosen, weekend_scaling, eventnumpermonth)
+        print("\nProbability to '%s' on a weekend %s" % (name, probabilities[0]))
+        print("Probability to '%s' on a weekday %s" % (name, probabilities[1]))
+        dates_chosen = select_days(probabilities, month_chosen)
+        print("\nDates chosen to '%s'" % name)
+        numeventsplanned += 1
+        eventnamevect.append(name)
+        eventdatevect.append(dates_chosen)
+    for event_idx in range(len(eventnamevect)):
+        name = eventnamevect[event_idx]
+        dates = eventdatevect[event_idx]
+        print("\n%s on..." % name)
+        for d in dates:
+            print("%s %s" % (calendar.month_name[month_chosen], d))
 
 
 if __name__ == "__main__":
