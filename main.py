338ea978c5d9174c28e29a05043d948ad8cf9cb2
luak <arya_kaul@g.harvard.edu>
2022-03-25 20:16:34 -0400

    add argparser + change weekend/weekday logic

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import datetime
 import calendar
 import numpy as np
 import random
+import argparse
+import sys
+import os
+
+def parse_args(args):
+   parser = argparse.ArgumentParser(description="check help!")
+   parser.add_argument("-o", "--output", dest="outputpath", required=False, help="where to dump schedule to. default is ~/.cache/YYYY-MM for the chosen year / month", type=str)
+   return parser.parse_args()
 
 def get_num_weekends(year, month, monthrange):
     num = 0
@@ -18,7 +26,7 @@ def get_probabilities(month,year, weekend_scaling, avg_events):
     range_in_given_month = calendar.monthrange(year,month)
     num_weekends = get_num_weekends(year,month,range_in_given_month)
     num_weekdays = range_in_given_month[1]-num_weekends
-    lhs = np.array([[num_weekends, num_weekdays], [-num_weekends, weekend_scaling*num_weekdays]])
+    lhs = np.array([[num_weekends, num_weekdays], [-num_weekends, weekend_scaling+num_weekdays]])
     rhs = np.array([avg_events,0])
     soln = np.linalg.solve(lhs, rhs)
     pr_weekend = soln[0]
@@ -44,11 +52,13 @@ def select_days(probabilities, month, year):
 
 def main():
     print("\nWelcome. \n")
-    year_chosen = None 
+    year_chosen = None
     while year_chosen == None:
         year_chosen = input(
-            "What year would you like to plan monthly events for? [1900-inf] \n\t"
+            "What year would you like to plan monthly events for? [1900-inf] (ENTER for curr. year) \n\t"
         )
+        if year_chosen == "":
+            year_chosen = datetime.datetime.now().year
         try:
             year_chosen = int(year_chosen)
             if year_chosen < 1900:
@@ -56,12 +66,15 @@ def main():
         except:
             print("Invalid option. Please choose a valid year between 1900 and infinity\n\n")
             year_chosen = None
+    print("Year chosen: %s\n" % year_chosen)
 
     month_chosen = None
     while month_chosen == None:
         month_chosen = input(
-            "What month would you like to plan recurring, random events for? [01-12] \n\t"
+            "What month would you like to plan recurring, random events for? [01-12] (ENTER for curr. month) \n\t"
         )
+        if month_chosen == "":
+            month_chosen = datetime.datetime.now().month
         try:
             month_chosen = int(month_chosen)
             if month_chosen > 12 or month_chosen < 1:
@@ -69,7 +82,7 @@ def main():
         except:
             print("Invalid option. Please choose a number between 1 and 12\n\n")
             month_chosen = None
-    print("Month chosen: %s" % calendar.month_name[month_chosen])
+    print("Month chosen: %s\n" % calendar.month_name[month_chosen])
 
     numeventsplanned = 1
     eventnamevect = []
@@ -113,7 +126,7 @@ def main():
         weekend_scaling = None
         while weekend_scaling == None:
             weekend_scaling = input(
-                "\nHow much larger should E(numevents | weekdend) be than E(numevents | weekday)? \n\t"
+                "\nHow much larger should E(numevents) | weekdend be than E(numevents) | weekday? \n\t"
             )
             try:
                 weekend_scaling = float(weekend_scaling)
@@ -131,14 +144,25 @@ def main():
         numeventsplanned += 1
         eventnamevect.append(name)
         eventdatevect.append(dates_chosen)
- 
+    arguments = parse_args(sys.argv[1:])
+    defaultpath = os.path.join(os.path.expanduser("~"), ".cache", str(year_chosen)+"-"+"{:02d}".format(month_chosen))
+    if arguments.outputpath == None:
+        arguments.outputpath = defaultpath
     for event_idx in range(len(eventnamevect)):
         name = eventnamevect[event_idx]
         dates = eventdatevect[event_idx]
         print("\n%s on..." % name)
         for d in dates:
             print("%s %s" % (calendar.month_name[month_chosen], d))
-
+    with open(arguments.outputpath, 'w') as f:
+        print("Schedule written to... %s" % arguments.outputpath)
+        for event_idx in range(len(eventnamevect)):
+            name = eventnamevect[event_idx]
+            dates = eventdatevect[event_idx]
+            f.write("%s\t" % name)
+            for d in dates:
+                if d == dates[-1]: f.write("%s-%s-%s\n" % (year_chosen, "{:02d}".format(month_chosen), "{:02d}".format(d)))
+                else: f.write("%s-%s-%s\t" % (year_chosen, "{:02d}".format(month_chosen), "{:02d}".format(d)))
 
 if __name__ == "__main__":
     main()
